a0c4d17d1ccd14bee9e7ccbe32b176b6b2eff5f0
everpe <ever.penab@autonoma.edu.co>
2024-11-24 20:04:38 -0500

    delcaration components and routing

--- src/app/feature/users/create-user/create-user.component.ts
+++ src/app/feature/users/create-user/create-user.component.ts
@@ -13,7 +13,6 @@ export class CreateUserComponent implements OnInit {
   ) {
   }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 
   /**
--- src/app/feature/users/users.module.ts
+++ src/app/feature/users/users.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UsersRoutingModule } from './users-routing.module';
+import { HomeUserComponent } from './home-user/home-user.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 
 @NgModule({
-
+declarations: [
+  NavBarComponent,
+  HomeUserComponent,
+  ListUsersComponent,
+  CreateUserComponent
+],
+imports:[
+  UsersRoutingModule
+]
 })
 export class UsersModule {
 }
